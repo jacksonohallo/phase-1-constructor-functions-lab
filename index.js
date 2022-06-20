fda88a93b8cf1362e99171d8c3f304e011cb3b81
jacksonohallo <jacksonohallo@gmail.com>
2022-06-20 06:18:28 +0300

    commit tree 6d9c4c464a76fc3c4cb2d6e896e2e0fb2c06b1d8

---
+++ index.js
@@ -0,0 +1,17 @@
+function   scooterconstructor(year, color, model){
+    this.year = year;
+    this.color = color;
+    this.model = model
+  }
+  
+  function driver(name, age, experience){
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+  }
+  
+  function pickupLocation(address, city){
+    this.address = address;
+    this.city = city;
+  
+  }
